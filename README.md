82c520d99b55c5c9d3fe66460a8ab58732ae1f48
Jesus Sanchez <jesussanchez@MacBook-Air-de-Jesus.local>
2024-02-22 17:36:14 -0600

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿# Pruebas para el parámetro firstName al crear un/a usuario/a en []
+- Necesitas tener instalados los paquetes pytest y request para ejecutar las pruebas.
+- Ejecuta todacds las pruebas con el comando pytest.
---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-e48c42c6-c9aa-4432-a1c5-82a3bfa31922.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
